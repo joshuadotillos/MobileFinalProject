fab8c28ea68eb479a9a99976e158a8b8613562a2
Redempto D Legaspi III <redempto.legaspi.20@usjr.edu.ph>
2020-12-15 02:18:49 +0800

    Main App setup

---
+++ mobile_final_project/lib/screens/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
